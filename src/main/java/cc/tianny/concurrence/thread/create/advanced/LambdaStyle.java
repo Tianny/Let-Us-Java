3c5fb659d5b143ae1bc2b09e574fe7a46b1c5461
Tianny <tiannyxie@gmail.com>
2021-02-24 16:27:34 +0800

    create thread examples

---
+++ src/main/java/cc/tianny/concurrence/thread/create/advanced/LambdaStyle.java
@@ -0,0 +1,14 @@
+package cc.tianny.concurrence.thread.create.advanced;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tianny
+ * Date: 2021/2/24
+ * Time: 3:41 下午
+ * Description: lambda 表达式创建线程
+ */
+public class LambdaStyle {
+    public static void main(String[] args) {
+        new Thread(() -> System.out.println(Thread.currentThread().getName())).start();
+    }
+}
